ed30bad2c7d76dce5433284ac15e719bccc1360e
Cédric PIERSON <cdric.pierson@gmail.com>
2022-10-11 15:36:21 +0200

    add Hello component + css

---
+++ src/components/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Hello.css';
+
+const Hello = () => {
+    return (
+        <div>
+            <h1 className="Build">Moi je bild</h1>
+        </div>
+    )
+}
+
+export default Hello;
